0eb3af862ee8d2f88cc40fc5ca67b1eae0d4486b
vbasoon <vbasoon@gmail.com>
2024-04-11 16:06:47 +0300

    configuration env

--- src/api/tmdb.ts
+++ src/api/tmdb.ts
@@ -1,14 +1,16 @@
+import configuration from "../configuration"
+
 async function get(relativeURL: string) {
   const options = {
         method: 'GET',
         headers: {
         accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMzE0Y2U3ZWExYTRhNzA1OTJkMTQyMGUwZmY0MTc4NCIsInN1YiI6IjY2MTc3MjU5OTBiODdlMDE2MzNkNGE4NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Q8rAoV3PUO8xOWaxlwOt5UL5-pLW14pItJSUVy35rqw'
+          Authorization: `Bearer ${configuration.apiTkn}`
         }
   };
 
   const response = await fetch(
-    `https://api.themoviedb.org/3${relativeURL}`, options);
+    `${configuration.apiUrl}/3${relativeURL}`, options);
 
   const json = response.json()
 
---
+++ src/configuration.ts
@@ -0,0 +1,6 @@
+const configuration = {
+  apiUrl: process.env.REACT_APP_API_URL,
+  apiTkn: process.env.REACT_APP_API_TKN,
+}
+
+export default configuration
