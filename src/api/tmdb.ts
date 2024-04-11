de22a2fa1392b036ac0f1e4f370a2599517a652c
vbasoon <vbasoon@gmail.com>
2024-04-11 16:16:26 +0300

    typising

--- src/api/tmdb.ts
+++ src/api/tmdb.ts
@@ -1,6 +1,6 @@
 import configuration from "../configuration"
 
-async function get(relativeURL: string) {
+async function get<TBody>(relativeURL: string): Promise<TBody> {
   const options = {
         method: 'GET',
         headers: {
@@ -12,13 +12,20 @@ async function get(relativeURL: string) {
   const response = await fetch(
     `${configuration.apiUrl}/3${relativeURL}`, options);
 
-  const json = response.json()
+  const json: TBody = await response.json()
 
   return json;
 }
 
+interface MovieDetails {
+  id: number,
+  title: string,
+  overview: string,
+  popularity: number,
+}
+
 export const client = {
-  async getPlayNow()  {
+  async getPlayNow(): Promise<MovieDetails[]>  {
   
   return get("/movie/now_playing?page=1")
     
