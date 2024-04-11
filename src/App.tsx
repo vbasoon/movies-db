41e8925ec81177aab7d8119b030b5629f5d0a5d4
vbasoon <vbasoon@gmail.com>
2024-04-11 11:17:57 +0300

    fetch TMDB

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
 import { Link, Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
+    <div className={styles.app}>
+      <header className={styles.header}>
+        <img src="/cinema-ico.png" className={styles.logo} alt="logo" />
+        <nav className={styles.menu}>
+          <ul className={styles.menu__list}>
+          <li className={styles.menu__item}>
+            <Link className={styles.menu__link} to="/">Home</Link>
           </li>
-          <li>
-            <Link to="/about">About</Link>
+          <li className={styles.menu__item}>
+            <Link className={styles.menu__link} to="/about">About</Link>
           </li>
-          <li>
-            <Link to="/movies">Movies</Link>
+          <li className={styles.menu__item}>
+            <Link className={styles.menu__link} to="/movies">Movies</Link>
           </li>
         </ul>
+        </nav>
+        
       </header>
-      <main className="App-main">
+      <main className={styles.main}>
         <Outlet/>
       </main>
     </div>
--- src/pages/Movies/Movies.tsx
+++ src/pages/Movies/Movies.tsx
@@ -5,6 +5,37 @@ import MovieCard from "./MovieCard";
 import styles from "./Movies.module.scss"
 
 import './Movies.css'
+import { useEffect, useState } from "react";
+
+async function getPlayNow()  {
+  const options = {
+        method: 'GET',
+        headers: {
+        accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMzE0Y2U3ZWExYTRhNzA1OTJkMTQyMGUwZmY0MTc4NCIsInN1YiI6IjY2MTc3MjU5OTBiODdlMDE2MzNkNGE4NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Q8rAoV3PUO8xOWaxlwOt5UL5-pLW14pItJSUVy35rqw'
+        }
+  };
+
+  const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', options)
+
+  const json = response.json()
+
+  return json;
+    
+}
+
+function MoviesFetch() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    async function loadData() {
+      const response = await getPlayNow();
+      setMovies(response.results)
+    }
+
+  loadData();
+  }, [])
+  return <Movies movies={movies} />
+}
 
 interface MoviesProps {
   movies: Movie[]
