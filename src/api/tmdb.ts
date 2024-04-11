9989113865b22bb2e368d00967787b4c714944f0
vbasoon <vbasoon@gmail.com>
2024-04-11 15:13:51 +0300

    client refactoring

--- src/api/tmdb.ts
+++ src/api/tmdb.ts
@@ -7,7 +7,8 @@ async function get(relativeURL: string) {
         }
   };
 
-  const response = await fetch(`https://api.themoviedb.org/3${relativeURL}`, options)
+  const response = await fetch(
+    `https://api.themoviedb.org/3${relativeURL}`, options);
 
   const json = response.json()
 
@@ -16,19 +17,8 @@ async function get(relativeURL: string) {
 
 export const client = {
   async getPlayNow()  {
-  const options = {
-        method: 'GET',
-        headers: {
-        accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMzE0Y2U3ZWExYTRhNzA1OTJkMTQyMGUwZmY0MTc4NCIsInN1YiI6IjY2MTc3MjU5OTBiODdlMDE2MzNkNGE4NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Q8rAoV3PUO8xOWaxlwOt5UL5-pLW14pItJSUVy35rqw'
-        }
-  };
-
-  const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', options)
-
-  const json = response.json()
-
-  return json;
+  
+  return get("/movie/now_playing?page=1")
     
 }
 }
