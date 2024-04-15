545bf623c5dfe5bf028399285a3e568b2d61258f
vbasoon <vbasoon@gmail.com>
2024-04-15 15:27:14 +0300

    implementation thunk

--- src/store/reducers/movies.ts
+++ src/store/reducers/movies.ts
@@ -1,6 +1,8 @@
 import { ActionWithPayload } from './../../redux/utils';
 import { createReducer } from '../../redux/utils';
 import { Reducer, Action } from "redux";
+import { AppThunk } from '../store';
+import { client } from '../../api/tmdb';
 
 export interface Movie {
   id: number,
@@ -28,6 +30,27 @@ export const moviesLoaded = (movies: Movie[]) => ({ // 1.Action creator & Action
   type: "movies/loaded",
   payload: movies
 })
+
+export function fetchMovies (): AppThunk<Promise<void>> {
+  return async (dispatch, getState) => {
+    dispatch(moviesLoading())
+      const config = await client.getConfiguration()
+      const imageUrl = config.images.base_url;
+      const results = await client.getPlayNow();
+
+      const mappedResults: Movie[] = results.map((m)=> ({
+        id: m.id,
+        title: m.title,
+        overview: m.overview,
+        popularity: m.popularity,
+        image: m.backdrop_path 
+          ? `${imageUrl}w780${m.backdrop_path}` 
+          : undefined,
+      }));
+
+      dispatch(moviesLoaded(mappedResults)); // dispatch loaded
+  }
+}
 
 const moviesReducer = createReducer<MovieState>(
   initialState,
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,23 @@
-import { createStore } from 'redux';
+import { UnknownAction, applyMiddleware, createStore } from 'redux';
 
 import rootReducer from "./reducers"
 
-const store = createStore(rootReducer)
+import { ThunkAction, thunk } from "redux-thunk"
+
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+const composeEnhanser = composeWithDevTools(applyMiddleware(thunk))
+
+const store = createStore(rootReducer, composeEnhanser)
+
+export type AppDispatch = typeof store.dispatch
+
+export type AppThunk<ReturnType> = ThunkAction<
+  ReturnType, 
+  RootState, 
+  undefined, 
+  UnknownAction
+>;
 
 export type RootState = ReturnType<typeof store.getState>
 
